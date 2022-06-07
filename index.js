9985fc1ec470d8e8bae2b24e3b83335e4480053d
KunjYadav <kunj.yadav555@gmail.com>
2022-06-07 22:51:40 +0530

    Expense Tracker Project

--- index.js
+++ index.js
@@ -1,113 +1,91 @@
-(function(){
 
-    // initial array of expenses, reading from localStorage
-    const expenses = JSON.parse(localStorage.getItem('expenses')) || [];
+function saveToLocalStorage(event) {
+  event.preventDefault();
+  const expenseamount = document.getElementById("amount").value;
+  const description = document.getElementById("description").value;
+  const category = document.getElementById("category").value;
 
-    document.getElementById('exp-Form').addEventListener('submit', function(e){
+    const obj = {
+        expenseamount,
+        description,
+        category //unique
 
-        e.preventDefault();
-
-        // get type, name, date, and amount
-        let type = document.getElementById('type').value;
-        let name = document.getElementById('name').value;
-        let date = document.getElementById('date').value;
-        let amount = document.getElementById('amount').value;
-
-        if(type == 'chooseOne' || name.length <= 0 || date == '' ) {return;}
-
-        const expense = {
-            type, 
-            name, 
-            date,
-            amount, 
-            id: expenses.length > 0 ? expenses[expenses.length - 1].id + 1 : 1,
-        }
-
-        expenses.push(expense);
-        // localStorage 
-        localStorage.setItem('expenses', JSON.stringify(expenses));
-
-        document.getElementById('exp-Form').reset();
-        showExpenses();
-    });
-
-    function showExpenses(){
-
-        const expenseTable = document.getElementById('expenseTable');
-        expenseTable.innerHTML = '';
-
-        if(expenses.length > 0){
-            for(let i = 0; i < expenses.length; i++){
-
-                expenseTable.appendChild(createDataRow(expenses[i])); 
-                
-            }  // end of for loop
-        }  else {
-            // expenses count is 0
-            expenseTable.appendChild(createEmptyRow());           
-        }
     }
 
-    function createEmptyRow(){
-        const expenseRowEl = document.createElement('TR');       
+    axios.post("https://crudcrud.com/api/87971a5ca67b4552831c6dc3ebb70b16/appointmentData", obj)
+    .then((response) => {
+      showNewUserOnScreen(response.data)
+      // console.log(response)
+    })
+    .catch((err) => {
+      document.body.innerHTML = document.body.innerHTML + "<h4> Something went wrong </h4>"
+      console.log(err)
+    })
+    // localStorage.setItem("userDetails" + description, JSON.stringify(object)); // localStorage.setItem("userDetailEmail" + emailId, emailId);
+    // // listOfPeople.push(object)
 
-        const expenseTdTypeEl = document.createElement('TD');
-        expenseTdTypeEl.setAttribute('colspan', 5);
-        expenseTdTypeEl.textContent = 
-            'No expense items yet! Please add one up top...';
-        expenseRowEl.appendChild(expenseTdTypeEl);
+    // addNewLineElement(object);
+  }
 
-        return expenseRowEl;
-    }
-
-    function createDataRow(expense){
-        
-        const expenseRowEl = document.createElement('TR');
-
-        const expenseTdTypeEl = document.createElement('TD');
-        expenseTdTypeEl.textContent = expense.type;
-        expenseRowEl.appendChild(expenseTdTypeEl);
-
-        const expenseTdNameEl = document.createElement('TD');
-        expenseTdNameEl.textContent = expense.name;
-        expenseRowEl.appendChild(expenseTdNameEl);
-
-        const expenseTdDateEl = document.createElement('TD');
-        expenseTdDateEl.textContent = expense.date;
-        expenseRowEl.appendChild(expenseTdDateEl);
-
-        const expenseTdAmountEl = document.createElement('TD');
-        expenseTdAmountEl.textContent = '$' + expense.amount;
-        expenseRowEl.appendChild(expenseTdAmountEl);
-
-        const expenseTdOptionsEl = document.createElement('TD');
-        const deleteAnchorEl = document.createElement('A');
-        deleteAnchorEl.className = "deleteButton";
-        deleteAnchorEl.onclick = function(e){
-            deleteExpense(expense.id);
-
-                // localStorage
-            localStorage.setItem('expenses', JSON.stringify(expenses));
-            showExpenses();
-        }
-
-        deleteAnchorEl.textContent = 'Delete';
-        expenseRowEl.appendChild(deleteAnchorEl);
-
-        return expenseRowEl;
-    }
-
-    function deleteExpense(id){
-        for(let i = 0; i < expenses.length; i++){
-            if(expenses[i].id == id){
-                expenses.splice(i, 1);
-            }
-        }
-
-       
-    }
-
-    showExpenses();
+  window.addEventListener("DOMContentLoaded", () => {
+    axios.get("https://crudcrud.com/api/87971a5ca67b4552831c6dc3ebb70b16/appointmentData")
+    .then((response) => {
+      console.log(response)
+  
+      for(var i=0; i<response.data.length; i++) {
+        showNewUserOnScreen(response.data[i])
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  })
 
 
-})();
+function showNewUserOnScreen(user){
+  
+  
+  const parentNode = document.getElementById('listOfPeople');
+  const childHTML = `<li id=${user._id}> ${user.amount} - ${user.description} - ${user.category}
+  <button onclick=deleteUser('${user._id}')> Delete Expense </button>
+  <button onclick=editUserDetails('${user.amount}', '${user.description}','${user.category}','${user._id}')>Edit Expense </button>
+  </li>`
+
+  parentNode.innerHTML = parentNode.innerHTML + childHTML;
+}
+
+//Edit User
+  
+function editUserDetails(amount, description, category, userId){
+  
+  document.getElementById("amount").value = amount;
+  document.getElementById("description").value = description;
+  document.getElementById("category").value = category;
+
+  deleteUser(userId)
+}
+
+
+// deleteUser('abc@gmail.com')
+function deleteUser(userId){
+  axios.delete(`https://crudcrud.com/api/87971a5ca67b4552831c6dc3ebb70b16/appointmentData/${userId}`)
+  .then((response) => {
+    removeUserFromScreen(userId)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+ 
+  // console.log(emailId)
+  // localStorage.removeItem(emailId);
+  // removeUserFromScreen(emailId);
+
+}
+
+function removeUserFromScreen(userId){
+  const parentNode = document.getElementById('listOfPeople');
+  const childNodeToBeDeleted = document.getElementById(userId);
+  if(childNodeToBeDeleted) {
+  parentNode.removeChild(childNodeToBeDeleted)
+  }
+}
